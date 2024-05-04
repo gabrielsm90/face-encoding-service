49352a48c8dc080e5a098a4c998959a76697b7db
Gabriel Menezes <gabrielsm90@gmail.com>
2024-05-04 13:17:17 +0200

    chore: [Linting] Enforce type annotations in mypy and exclude tests from mypy check

--- src/controllers/SystemController.py
+++ src/controllers/SystemController.py
@@ -1,13 +1,15 @@
+from typing import Dict
+
 from fastapi import APIRouter
 
 router = APIRouter(tags=["System"])
 
 
 @router.get("/")
-def index():
+def index() -> Dict[str, str]:
     return {"message": "Welcome to Veriff's Face Encoding Service!"}
 
 
 @router.get("/health")
-def health():
+def health() -> Dict[str, str]:
     return {"status": "ok"}
